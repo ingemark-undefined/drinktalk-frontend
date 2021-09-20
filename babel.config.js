165e7cc1b8c50e0b5772b2bbfa17c7fe4816474e
Jakov Glavina <jakov.glavina@icloud.com>
2021-09-20 13:43:16 +0200

    chore: initial project structure setup, set up
    module path resolving

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js', '.json', '.svg', '.ttf'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@screens': './src/screens',
+        },
+      },
+    ],
+  ],
 };
