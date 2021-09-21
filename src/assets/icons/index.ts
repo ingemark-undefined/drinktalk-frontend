039774b849e75a18673a4f6f6c2cdd609071c358
Jakov Glavina <jakov.glavina@icloud.com>
2021-09-21 20:36:14 +0200

    feat: add chevron up/down icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,4 @@
 export { default as LogoIcon } from './logo.svg';
 export { default as ChevronLeftIcon } from './chevron-left.svg';
+export { default as ChevronUpIcon } from './chevron-up.svg';
+export { default as ChevronDownIcon } from './chevron-down.svg';
