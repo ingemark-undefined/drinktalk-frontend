5209ec0399a7192401b214c891b0661a075420e4
fifi98 <bel.filip@icloud.com>
2021-09-22 20:16:14 +0200

    feat: add Instructions component

---
+++ src/components/Instructions.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { fontSize } from '@constants/typography';
+
+interface InstructionsProps {}
+
+const Instructions: React.FunctionComponent<InstructionsProps> = () => {
+  return (
+    <View>
+      <Text style={[styles.text, styles.margin]}>
+        Ukoliko podigneš mobitel u sljedećih <Text style={styles.bold}>1 sat 30 min</Text> ostalim sudionicima igre doći će notifikacija da
+        si izgubio.
+      </Text>
+      <Text style={styles.bold}>Budi fer i plati piće ako izgubiš.</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontFamily: 'GothamRounded',
+    fontSize: fontSize.medium,
+    textAlign: 'center',
+  },
+  bold: {
+    fontFamily: 'GothamRoundedBold',
+  },
+  margin: {
+    marginBottom: 30,
+  },
+});
+
+export default Instructions;
