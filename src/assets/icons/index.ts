f249e78146df14637c458160944fc1f70d71868f
Jakov Glavina <jakov.glavina@icloud.com>
2021-09-22 20:54:10 +0200

    feat: add CloseIcon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,3 +2,4 @@ export { default as LogoIcon } from './logo.svg';
 export { default as ChevronLeftIcon } from './chevron-left.svg';
 export { default as ChevronUpIcon } from './chevron-up.svg';
 export { default as ChevronDownIcon } from './chevron-down.svg';
+export { default as CloseIcon } from './close.svg';
