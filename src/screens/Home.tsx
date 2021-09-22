6e3ca92da410d07822549a678766e293dba7d580
Jakov Glavina <jakov.glavina@icloud.com>
2021-09-23 00:52:36 +0200

    feat: background actions - experiment

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, StyleSheet, View, TouchableOpacity } from 'react-native';
 import { RouteProp, useNavigation } from '@react-navigation/native';
+import BackgroundService from 'react-native-background-actions';
 
 import { BottomButton, CircleButton, Icon, Screen } from '@components/index';
 
@@ -10,6 +11,7 @@ import { fontSize } from '@constants/typography';
 import colors from '@constants/colors';
 import screen from '@navigation/screens';
 import { StackNavigationProp } from '@react-navigation/stack';
+import { accelerometer, setUpdateIntervalForType, SensorTypes } from 'react-native-sensors';
 
 type HomeScreenRouteProp = RouteProp<NavigatorParamList, screen.HOME>;
 type HomeScreenNavigationProp = StackNavigationProp<NavigatorParamList, screen.HOME>;
@@ -18,9 +20,43 @@ interface HomeProps {
   route: HomeScreenRouteProp;
 }
 
+const veryIntensiveTask = async (taskDataArguments) => {
+  // Example of an infinite loop task
+  await new Promise(async (resolve) => {
+    setUpdateIntervalForType(SensorTypes.accelerometer, 100);
+    const subscription = accelerometer.subscribe(({ x, y, z, timestamp }) => console.log({ x, y, z, timestamp }));
+  });
+};
+
+const options = {
+  taskName: 'Example',
+  taskTitle: 'ExampleTask title',
+  taskDesc: 'ExampleTask description',
+  taskIcon: {
+    name: 'ic_launcher',
+    type: 'mipmap',
+  },
+  color: '#ff00ff',
+  linkingURI: 'yourSchemeHere://chat/jane', // See Deep Linking for more info
+  parameters: {
+    delay: 1000,
+  },
+};
+
 const Home: React.FunctionComponent<HomeProps> = ({ route }) => {
   const navigation = useNavigation<HomeScreenNavigationProp>();
   const { name } = route.params;
+
+  useEffect(() => {
+    const zapocni = async () => {
+      try {
+        await BackgroundService.start(veryIntensiveTask, options);
+      } catch (err) {
+        console.log('error', err);
+      }
+    };
+    zapocni();
+  }, []);
 
   return (
     <Screen style={styles.container}>
