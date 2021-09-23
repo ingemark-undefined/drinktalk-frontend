3ff4e6ed7c95e40d91f239a38fee1de1f927ade8
fifi98 <bel.filip@icloud.com>
2021-09-24 01:38:06 +0200

    feat: add loading prop

--- src/components/CircleButton.tsx
+++ src/components/CircleButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet, GestureResponderEvent, ViewStyle, TextStyle } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet, GestureResponderEvent, ViewStyle, TextStyle, ActivityIndicator } from 'react-native';
 
 import colors from '@constants/colors';
 import { fontSize } from '@constants/typography';
@@ -10,9 +10,10 @@ interface CircleButtonProps {
   onPress: (event: GestureResponderEvent) => void;
   style?: ViewStyle;
   textStyle?: TextStyle;
+  loading?: boolean;
 }
 
-const CircleButton: React.FunctionComponent<CircleButtonProps> = ({ size = 80, title, onPress, style, textStyle }) => {
+const CircleButton: React.FunctionComponent<CircleButtonProps> = ({ size = 80, title, onPress, style, textStyle, loading }) => {
   const buttonStyle = {
     width: size,
     height: size,
@@ -21,7 +22,7 @@ const CircleButton: React.FunctionComponent<CircleButtonProps> = ({ size = 80, t
 
   return (
     <TouchableOpacity style={[styles.container, buttonStyle, style]} onPress={onPress}>
-      <Text style={[styles.text, textStyle]}>{title}</Text>
+      {loading ? <ActivityIndicator color={colors.white} size="large" /> : <Text style={[styles.text, textStyle]}>{title}</Text>}
     </TouchableOpacity>
   );
 };
