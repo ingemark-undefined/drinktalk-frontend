5edb6b36623f7b11c7cccf825722956b7f6336a2
fifi98 <bel.filip@icloud.com>
2021-09-23 02:07:14 +0200

    feat: go to game screen on countdown finish

--- src/screens/Countdown.tsx
+++ src/screens/Countdown.tsx
@@ -3,17 +3,36 @@ import { Animated, StyleSheet, Text, View } from 'react-native';
 import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
 
 import { Screen, Instructions } from '@components/index';
+import screen from '@navigation/screens';
 
 import { fontSize } from '@constants/typography';
 import colors from '@constants/colors';
+import { useNavigation } from '@react-navigation/core';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { NavigatorParamList } from '@navigation/Navigator';
+
+type CountdownScreenNavigationProp = StackNavigationProp<NavigatorParamList, screen.COUNTDOWN>;
 
 interface CountdownProps {}
 
 const Countdown: React.FunctionComponent<CountdownProps> = () => {
+  const navigation = useNavigation<CountdownScreenNavigationProp>();
+
+  const handleStart = () => {
+    navigation.replace(screen.GAME);
+  };
+
   return (
     <Screen style={styles.container}>
       <Text style={styles.text}>Spusti i ne diraj mobitel za</Text>
-      <CountdownCircleTimer isPlaying duration={10} trailStrokeWidth={0} strokeWidth={10} size={200} colors={colors.white}>
+      <CountdownCircleTimer
+        isPlaying
+        duration={10}
+        trailStrokeWidth={0}
+        strokeWidth={10}
+        size={200}
+        colors={colors.white}
+        onComplete={handleStart}>
         {({ remainingTime }) => <Animated.Text style={styles.countdownText}>{remainingTime}</Animated.Text>}
       </CountdownCircleTimer>
       <Text style={styles.text}>sekundi</Text>
