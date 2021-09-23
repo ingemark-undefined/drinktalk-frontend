484371d484231e25606793757b68f612bdb846b3
fifi98 <bel.filip@icloud.com>
2021-09-24 00:47:19 +0200

    feat: go to the home screen on button click

--- src/screens/Success.tsx
+++ src/screens/Success.tsx
@@ -1,28 +1,25 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
-import { RouteProp } from '@react-navigation/native';
 
 import { Screen, BottomButton, Card } from '@components/index';
 
-import { NavigatorParamList } from '@navigation/Navigator';
+import { useNavigation } from '@react-navigation/core';
 import { fontSize } from '@constants/typography';
 import colors from '@constants/colors';
 import screen from '@navigation/screens';
 
-type SuccessScreenRouteProp = RouteProp<NavigatorParamList, screen.HOME>;
-
-interface SuccessProps {
-  route: SuccessScreenRouteProp;
-}
+interface SuccessProps {}
 
 const Success: React.FunctionComponent<SuccessProps> = () => {
+  const navigation = useNavigation();
+
   return (
     <Screen style={styles.container}>
       <Card>
         <Text style={[styles.text, styles.title]}>Bravo!</Text>
         <Text style={[styles.text, styles.subtitle]}>Vi ste prava ekipa koja može zaboraviti tehnologiju na tren.</Text>
       </Card>
-      <BottomButton title="Nova igra?" />
+      <BottomButton title="Nova igra?" onPress={() => navigation.replace(screen.HOME)} />
     </Screen>
   );
 };
