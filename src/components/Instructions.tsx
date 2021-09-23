69a9fd9e61a01987adc9a65d259ef242c42d762c
fifi98 <bel.filip@icloud.com>
2021-09-23 14:10:39 +0200

    feat: show actual game time

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { useAppSelector } from '@redux/hooks';
 import { fontSize } from '@constants/typography';
+import timeFormat from '@utils/timeFormat';
 
 interface InstructionsProps {}
 
 const Instructions: React.FunctionComponent<InstructionsProps> = () => {
+  const { time } = useAppSelector((state) => state.game);
+
   return (
     <View>
       <Text style={[styles.text, styles.margin]}>
-        Ukoliko podigneš mobitel u sljedećih <Text style={styles.bold}>1 sat 30 min</Text> ostalim sudionicima igre doći će notifikacija da
-        si izgubio.
+        Ukoliko podigneš mobitel u sljedećih <Text style={styles.bold}>{timeFormat(time, ' sat', ' min')}</Text> ostalim sudionicima igre
+        doći će notifikacija da si izgubio.
       </Text>
       <Text style={styles.bold}>Budi fer i plati piće ako izgubiš.</Text>
     </View>
